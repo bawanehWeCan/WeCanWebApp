d6cee233d7f7cfa83816a689496e406b6c2e056c
Alaa Bawaneh <a.bawaneh@wecan.jo>
2022-09-19 17:34:06 +0300

    upload

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
     <link rel="apple-touch-icon" href="{{ asset('images/favicon/icon-96x96.png') }}">
 
     <!-- CSS FILES -->
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css?v=11') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css?v=12') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/remixicon.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/vendors/swiper/swiper-bundle.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/vendors/zuck_stories/zuck.min.css') }}">
@@ -55,8 +55,8 @@
     <!-- START WRAPPER -->
     <div id="wrapper">
         <div class="c-form show-d">
-           
-            
+
+
             <section class="about-section">
                 <div class="un-navMenu-default without-icon py-0">
                     <ul class="nav flex-column">
