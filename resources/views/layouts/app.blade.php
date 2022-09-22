32894715c4a0a22a1a71299e3260943a428e7e3e
Alaa Bawaneh <bawanehcoder@live.com>
2022-09-22 22:59:24 +0300

    big update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
     <link rel="apple-touch-icon" href="{{ asset('images/favicon/icon-96x96.png') }}">
 
     <!-- CSS FILES -->
-    <link rel="stylesheet"  href="{{ asset('assets/css/style.css?v=27') }}">
+    <link rel="stylesheet"  href="{{ asset('assets/css/style.css?v=28') }}">
     <link rel="stylesheet"  href="{{ asset('assets/css/remixicon.min.css') }}">
     <link rel="stylesheet"  href="{{ asset('assets/vendors/swiper/swiper-bundle.min.css?v=1') }}">
     <link rel="stylesheet"  href="{{ asset('assets/vendors/zuck_stories/zuck.min.css?') }}">
