c0f7cbbe4bd99d443a1a472390fa705aef118332
Alaa Bawaneh <a.bawaneh@wecan.jo>
2022-09-26 17:53:26 +0300

    11

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -1,11 +1,354 @@
 @extends('layouts.home')
 
 @section('content')
+<div class="unSwiper-cards bg-white py-3">
+    <div class="content-cards-NFTs">
+        <div class="swiper cardGradual">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <!-- item-card-gradual -->
+                    <div class="item-card-gradual">
+                        <!-- <div class="head-card"></div> -->
+                        <a href="page-collectibles-details.html" class="body-card">
+                            <div class="cover-nft">
+                                <picture>
+                                    <source srcset="images/other/26.jpg" type="image/webp">
+                                    <img class="img-cover" src="images/other/26.jpg"
+                                        alt="image NFT">
+                                </picture>
+
+                                <div class="icon-type">
+                                    <i class="ri-vidicon-line"></i>
+                                </div>
+                                <div class="countdown-time">
+                                    <span>08H 38M 16S</span>
+                                </div>
+                            </div>
+                            <div class="title-card-nft">
+                                <div class="side-one">
+                                    <h2>The Dark Corner</h2>
+                                    <p>8 Editions Minted</p>
+                                </div>
+                                <div class="side-other">
+                                    <span class="no-sales">3 for sale</span>
+                                </div>
+                            </div>
+                            <div class="creator-name">
+                                <div class="image-user">
+                                    <picture>
+                                        <source srcset="images/avatar/14.jpg" type="image/webp">
+                                        <img class="img-avatar" src="images/avatar/14.jpg" alt="">
+                                    </picture>
+                                    <div class="icon">
+                                        <i class="ri-checkbox-circle-fill"></i>
+                                    </div>
+                                </div>
+                                <h3>Settimio Loggia</h3>
+                            </div>
+                        </a>
+                        <div class="footer-card">
+                            <div class="starting-bad">
+                                <h4>2.78 ETH</h4>
+                                <span>Starting Bid</span>
+                            </div>
+                            <div class="btn-like-click">
+                                <div class="btnLike">
+                                    <input type="checkbox">
+                                    <span class="count-likes">195</span>
+                                    <i class="ri-heart-3-line"></i>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <!-- item-card-gradual -->
+                    <div class="item-card-gradual">
+                        <!-- <div class="head-card"></div> -->
+                        <a href="page-collectibles-details.html" class="body-card">
+                            <div class="cover-nft">
+                                <picture>
+                                    <source srcset="images/other/14.jpg" type="image/webp">
+                                    <img class="img-cover" src="images/other/14.jpg"
+                                        alt="image NFT">
+                                </picture>
+                                <div class="countdown-time">
+                                    <span>08H 38M 16S</span>
+                                </div>
+                            </div>
+                            <div class="title-card-nft">
+                                <div class="side-one">
+                                    <h2>Galaxy on Earth</h2>
+                                    <p>6 Editions Minted</p>
+                                </div>
+                                <div class="side-other">
+                                    <span class="no-sales">2 for sale</span>
+                                </div>
+                            </div>
+                            <div class="creator-name">
+                                <div class="image-user">
+                                    <picture>
+                                        <source srcset="images/avatar/21.jpg" type="image/webp">
+                                        <img class="img-avatar" src="images/avatar/21.jpg" alt="">
+                                    </picture>
+                                    <div class="icon">
+                                        <i class="ri-checkbox-circle-fill"></i>
+                                    </div>
+                                </div>
+                                <h3>Leda Beneventi</h3>
+                            </div>
+                        </a>
+                        <div class="footer-card">
+                            <div class="starting-bad">
+                                <h4>2.40 ETH</h4>
+                                <span>Starting Bid</span>
+                            </div>
+                            <div class="btn-like-click">
+                                <div class="btnLike">
+                                    <input type="checkbox" checked>
+                                    <span class="count-likes">164</span>
+                                    <i class="ri-heart-3-line"></i>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <!-- item-card-gradual -->
+                    <div class="item-card-gradual">
+                        <!-- <div class="head-card"></div> -->
+                        <a href="page-collectibles-details.html" class="body-card">
+                            <div class="cover-nft">
+                                <picture>
+                                    <source srcset="images/other/27.jpg" type="image/webp">
+                                    <img class="img-cover" src="images/other/27.jpg"
+                                        alt="image NFT">
+                                </picture>
+                                <div class="countdown-time">
+                                    <span>08H 38M 16S</span>
+                                </div>
+                            </div>
+                            <div class="title-card-nft">
+                                <div class="side-one">
+                                    <h2>The Scary Shib</h2>
+                                    <p>8 Editions Minted</p>
+                                </div>
+                                <div class="side-other">
+                                    <span class="no-sales">3 for sale</span>
+                                </div>
+                            </div>
+                            <div class="creator-name">
+                                <div class="image-user">
+
+                                    <picture>
+                                        <source srcset="images/avatar/13.jpg" type="image/webp">
+                                        <img class="img-avatar" src="images/avatar/13.jpg" alt="">
+                                    </picture>
+                                    <div class="icon">
+                                        <i class="ri-checkbox-circle-fill"></i>
+                                    </div>
+                                </div>
+                                <h3>Bruce Wheless</h3>
+                            </div>
+                        </a>
+                        <div class="footer-card">
+                            <div class="starting-bad">
+                                <h4>1.27 ETH</h4>
+                                <span>Starting Bid</span>
+                            </div>
+                            <div class="btn-like-click">
+                                <div class="btnLike">
+                                    <input type="checkbox">
+                                    <span class="count-likes">95</span>
+                                    <i class="ri-heart-3-line"></i>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <!-- item-card-gradual -->
+                    <div class="item-card-gradual">
+                        <!-- <div class="head-card"></div> -->
+                        <a href="page-collectibles-details.html" class="body-card">
+                            <div class="cover-nft">
+                                <picture>
+                                    <source srcset="images/other/16.jpg" type="image/webp">
+                                    <img class="img-cover" src="images/other/16.jpg"
+                                        alt="image NFT">
+                                </picture>
+                                <div class="icon-type">
+                                    <i class="ri-vidicon-line"></i>
+                                </div>
+
+                            </div>
+                            <div class="title-card-nft">
+                                <div class="side-one">
+                                    <h2>The Dark Corner</h2>
+                                    <p>25 Editions Minted</p>
+                                </div>
+                                <div class="side-other">
+                                    <span class="no-sales">5 for sale</span>
+                                </div>
+                            </div>
+                            <div class="creator-name">
+                                <div class="image-user">
+
+                                    <picture>
+                                        <source srcset="images/avatar/17.jpg" type="image/webp">
+                                        <img class="img-avatar" src="images/avatar/17.jpg" alt="">
+                                    </picture>
+                                    <div class="icon">
+                                        <i class="ri-checkbox-circle-fill"></i>
+                                    </div>
+                                </div>
+                                <h3>Steve Jones</h3>
+                            </div>
+                        </a>
+                        <div class="footer-card">
+                            <div class="starting-bad">
+                                <h4>1.29 ETH</h4>
+                                <span>Starting Bid</span>
+                            </div>
+                            <div class="btn-like-click">
+                                <div class="btnLike">
+                                    <input type="checkbox">
+                                    <span class="count-likes">195</span>
+                                    <i class="ri-heart-3-line"></i>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <!-- item-card-gradual -->
+                    <div class="item-card-gradual">
+                        <!-- <div class="head-card"></div> -->
+                        <a href="page-collectibles-details.html" class="body-card">
+                            <div class="cover-nft">
+                                <picture>
+                                    <source srcset="images/other/21.jpg" type="image/webp">
+                                    <img class="img-cover" src="images/other/21.jpg"
+                                        alt="image NFT">
+                                </picture>
+                                <div class="countdown-time">
+                                    <span>08H 38M 16S</span>
+                                </div>
+                            </div>
+                            <div class="title-card-nft">
+                                <div class="side-one">
+                                    <h2>Ecstasy of the Dead</h2>
+                                    <p>350 Editions Minted</p>
+                                </div>
+                                <div class="side-other">
+                                    <span class="no-sales">9 for sale</span>
+                                </div>
+                            </div>
+                            <div class="creator-name">
+                                <div class="image-user">
+                                    <picture>
+                                        <source srcset="images/avatar/19.jpg" type="image/webp">
+                                        <img class="img-avatar" src="images/avatar/19.jpg" alt="">
+                                    </picture>
+                                    <div class="icon">
+                                        <i class="ri-checkbox-circle-fill"></i>
+                                    </div>
+                                </div>
+                                <h3>Hunter Taylor</h3>
+                            </div>
+                        </a>
+                        <div class="footer-card">
+                            <div class="starting-bad">
+                                <h4>1.79 ETH</h4>
+                                <span>Starting Bid</span>
+                            </div>
+                            <div class="btn-like-click">
+                                <div class="btnLike">
+                                    <input type="checkbox">
+                                    <span class="count-likes">297</span>
+                                    <i class="ri-heart-3-line"></i>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <!-- item-card-gradual -->
+                    <div class="item-card-gradual">
+                        <!-- <div class="head-card"></div> -->
+                        <a href="page-collectibles-details.html" class="body-card">
+                            <div class="cover-nft">
+                                <picture>
+                                    <source srcset="images/other/6.jpg" type="image/webp">
+                                    <img class="img-cover" src="images/other/6.jpg" alt="image NFT">
+                                </picture>
+                                <div class="icon-type">
+                                    <i class="ri-vidicon-line"></i>
+                                </div>
+                                <div class="countdown-time">
+                                    <span>08H 38M 16S</span>
+                                </div>
+                            </div>
+                            <div class="title-card-nft">
+                                <div class="side-one">
+                                    <h2>The Moon Boi</h2>
+                                    <p>14 Editions Minted</p>
+                                </div>
+                                <div class="side-other">
+                                    <span class="no-sales">2 for sale</span>
+                                </div>
+                            </div>
+                            <div class="creator-name">
+                                <div class="image-user">
+                                    <picture>
+                                        <source srcset="images/avatar/18.jpg" type="image/webp">
+                                        <img class="img-avatar" src="images/avatar/18.jpg" alt="">
+                                    </picture>
+                                    <div class="icon">
+                                        <i class="ri-checkbox-circle-fill"></i>
+                                    </div>
+                                </div>
+                                <h3>Craig Leach</h3>
+                            </div>
+                        </a>
+                        <div class="footer-card">
+                            <div class="starting-bad">
+                                <h4>2.78 ETH</h4>
+                                <span>Starting Bid</span>
+                            </div>
+                            <div class="btn-like-click">
+                                <div class="btnLike">
+                                    <input type="checkbox">
+                                    <span class="count-likes">195</span>
+                                    <i class="ri-heart-3-line"></i>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+        </div>
+    </div>
+</div>
     <!--
-                ===================================
-                         START THE HEADER
-                ===================================
-                -->
+                    ===================================
+                             START THE HEADER
+                    ===================================
+                    -->
     <header class="default heade-sticky">
         <div class="un-title-page go-back">
             <a href="javascript:history.back()" class="icon">
@@ -50,8 +393,8 @@
 
     <div class="space-sticky"></div>
     <!-- ===================================
-    START THE ACTIIVITY PAGE
-    ==================================== -->
+        START THE ACTIIVITY PAGE
+        ==================================== -->
     <section class="un-details-collectibles">
         <!-- head -->
         <div class="head">
