29f4f19005de3b5b32b13292bfd9215b44570b51
Alaa Bawaneh <a.bawaneh@wecan.jo>
2022-09-26 18:02:55 +0300

    11

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -1,6 +1,48 @@
 @extends('layouts.home')
 
 @section('content')
+ <!--
+                        ===================================
+                                 START THE HEADER
+                        ===================================
+                        -->
+                        <header class="default heade-sticky">
+                            <div class="un-title-page go-back">
+                                <a href="javascript:history.back()" class="icon">
+                                    <i class="ri-arrow-drop-right-line"></i>
+                                </a>
+                                <h1>{{ $project->name }}</h1>
+                            </div>
+
+                            <div class="un-block-right">
+
+                                <div class="un-user-profile">
+                                    <div class="em_darkMode_menu">
+
+                                        <a href="{{ url('/') }}">
+                                            <div class="btn btnCircle_default">
+                                                <i class="ri-home-5-line"></i>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                                <!-- menu-sidebar -->
+                                <div class="menu-sidebar">
+                                    <button type="button" name="sidebarMenu" aria-label="sidebarMenu" class="btn" data-bs-toggle="modal"
+                                        data-bs-target="#mdllSidebar-connected">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="19" height="9.3" viewBox="0 0 19 9.3">
+                                            <g id="Group_8081" data-name="Group 8081" transform="translate(-329 -37)">
+                                                <rect id="Rectangle_3986" data-name="Rectangle 3986" width="19" height="2.3"
+                                                    rx="1.15" transform="translate(329 37)" fill="#222032" />
+                                                <rect id="Rectangle_3987" data-name="Rectangle 3987" width="19" height="2.3"
+                                                    rx="1.15" transform="translate(329 44)" fill="#222032" />
+                                            </g>
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div>
+                        </header>
+
     <div class="space-items"></div>
 
     <div class="bg-white pt-3 pb-5">
@@ -151,47 +193,6 @@
         </div>
     </div>
     <div class="space-items"></div>
-    <!--
-                        ===================================
-                                 START THE HEADER
-                        ===================================
-                        -->
-    <header class="default heade-sticky">
-        <div class="un-title-page go-back">
-            <a href="javascript:history.back()" class="icon">
-                <i class="ri-arrow-drop-right-line"></i>
-            </a>
-            <h1>{{ $project->name }}</h1>
-        </div>
-
-        <div class="un-block-right">
-
-            <div class="un-user-profile">
-                <div class="em_darkMode_menu">
-
-                    <a href="{{ url('/') }}">
-                        <div class="btn btnCircle_default">
-                            <i class="ri-home-5-line"></i>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <!-- menu-sidebar -->
-            <div class="menu-sidebar">
-                <button type="button" name="sidebarMenu" aria-label="sidebarMenu" class="btn" data-bs-toggle="modal"
-                    data-bs-target="#mdllSidebar-connected">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="19" height="9.3" viewBox="0 0 19 9.3">
-                        <g id="Group_8081" data-name="Group 8081" transform="translate(-329 -37)">
-                            <rect id="Rectangle_3986" data-name="Rectangle 3986" width="19" height="2.3"
-                                rx="1.15" transform="translate(329 37)" fill="#222032" />
-                            <rect id="Rectangle_3987" data-name="Rectangle 3987" width="19" height="2.3"
-                                rx="1.15" transform="translate(329 44)" fill="#222032" />
-                        </g>
-                    </svg>
-                </button>
-            </div>
-        </div>
-    </header>
 
 
 
